4408f55bf81936f7a68eb097420d855115ea81c4
anthonydavisCA <adavis@crimsonagility.com>
2024-12-02 14:57:59 -0700

    fix mobile menu not loading

--- assets/theme.js
+++ assets/theme.js
@@ -16,9 +16,9 @@
         halo.ready();
     });
 
-    window.onload = function() { 
+    document.addEventListener("DOMContentLoaded", function () {
         halo.init();
-    }
+    });
 
     var halo = {
         haloTimeout: null,
